d334413fea7ec6d63b0a8aa36d04166dced923ec
x-god-lab <1763495066@qq.com>
2020-11-28 00:21:43 +0800

    Eureka项目集群配置，防止一个Eureka注册中心崩了，剩下还有

--- spring-cloud/springcloud-eureka-7001/Eureka.md
+++ spring-cloud/springcloud-eureka-7001/Eureka.md
@@ -4,3 +4,4 @@
 * 对Eureka进行相关配置
 * 启动类上开启Eureka的启动
 * 将项目相关注册到Eureka上
+* 使用Eureka集群，防止一个Eureka注册中心崩了，剩下还有其他的
---
+++ spring-cloud/springcloud-eureka-7002/src/main/java/com/xin/EurekaServer_7002.java
@@ -0,0 +1,18 @@
+package com.xin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xin
+ * @create 2020-11-27 18:02:16
+ */
+//启动之后访问 http://localhost:7001/
+@SpringBootApplication
+@EnableEurekaServer  //EnableEurekaServer服务端的启动刘，可以接受别人注册进来
+public class EurekaServer_7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7002.class,args);
+    }
+}
